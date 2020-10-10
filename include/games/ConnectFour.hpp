66751ceb8f57f3e88751a211caedc70ff35051c7
Antonio Román <kyradiscord@gmail.com>
2020-10-10 15:03:08 +0200

    fix: resolved build errors

--- include/games/ConnectFour.hpp
+++ include/games/ConnectFour.hpp
@@ -6,10 +6,10 @@
 
 namespace ConnectFour
 {
-	static constexpr uint_fast32_t board_width = 7Ui32;
-	static constexpr uint_fast32_t board_height = 6Ui32;
+	static constexpr uint_fast32_t board_width = 7U;
+	static constexpr uint_fast32_t board_height = 6U;
 
-	static constexpr uint_fast32_t board_min = 0Ui32;
+	static constexpr uint_fast32_t board_min = 0U;
 	static constexpr uint_fast32_t board_max = board_width * board_height;
 	typedef std::array<Players, board_max> ai_board;
 
